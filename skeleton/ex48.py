e77a79aea8ebe2fe52585be8ea5ade1073a2c42f
ginny <pig@ginny.com>
2017-06-24 20:51:03 +0800

    词汇扫描器

---
+++ skeleton/ex48.py
@@ -0,0 +1,36 @@
+def scan(stuff):
+    sentence = []
+    directions = ['north','south','east']
+    verbs = ['go','kill','eat']
+    stops = ['in','of','the']
+    nouns = ['bear','princess']
+    numbers = [3,91234,1234]
+    errors = ['IAS','ASDFADFASDF']
+    words = stuff.split()
+
+    for word in words:
+        try:
+            intword = int(word)
+            sentence.append(('number',int(word)))
+        except ValueError:
+
+            if word in directions:
+                sentence.append(('direction',word))
+            elif word in verbs:
+                sentence.append(('verb',word))
+            elif word in stops:
+                sentence.append(('stop',word))
+            elif word in nouns:
+                sentence.append(('noun',word))
+            elif word in errors:
+                sentence.append(('error',word))        
+            else:
+                sentence.append(('unkown',word))
+    return sentence
+
+
+print (scan("go north"))
+print (scan("kill the princess"))
+print (scan("eat the bear"))
+print (scan("open the door and smack the bear in the nose"))
+print (scan("open 1234 door"))
--- skeleton/setup.py
+++ skeleton/setup.py
@@ -13,7 +13,7 @@ config={
 	'install_requires': ['nose'],#代替require函数。表示当前包的安装依赖于哪些分发包，这些信息会写入egg的元信息中，这些包在安装时会自动（从PyPI）下载并安装。如果包在PyPI中找不到，则会从dependency_links标识的URL中获取。
 	'packages': ['NAME'],#告诉Distutils需要处理那些包（包含__init__.py的文件夹）（when you say packages = ['foo'] in your setup script, you are promising that the Distutils will find a file foo/__init__.py）
 	'scripts': [],#脚本名称
-	'name':'game.py' #项目名称
+	'name':'ex48a.py' #项目名称
 }
 
 setup(**config)
